b9da5b500918f892f9ccef43fa7a46dd1603740f
sarora2200 <sarora2200@gmail.com>
2024-04-16 18:24:00 +0000

    done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -10,13 +10,13 @@ export default function cleanSet(set, startString) {
   const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
-}
+  }
   for (const value of set.values()) {
-    if (typeof value == 'string' && value.startsWith(startString)) {
-      const valueSubStr = value.substring(startString.length); //cut after the startstring
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== value) {
-	 parts.push(valueSubStr);
+        parts.push(valueSubStr);
       }
     }
   }
